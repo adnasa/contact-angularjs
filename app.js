b378ce459ffe3688e203339bf55850c5b17d3011
Adnan Asani <adnan.asani@gmail.com>
2013-05-05 22:56:36 +0200

    Made a little change inspired by fabsor\'s socialnode

--- app.js
+++ app.js
@@ -7,32 +7,52 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , emitter = require('events').EventEmitter;
 
-var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+var app = express(),
+    Handler = new emitter;
+
+function Main() {
+  app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
+  });
+
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+  app.get('/', routes.index);
+  app.get('/users', user.list);
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
+    Handler.emit('serverLoaded');
+  });
+}
+
+var MainExtendables_ = {
+  loadConfigurations : function() {
+    console.log('loading configs...');
+  }
+}
+
+Main.prototype = MainExtendables_;
+
+Handler.on('serverLoaded', function() {
+  main.loadConfigurations();  
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+var main = new Main();
